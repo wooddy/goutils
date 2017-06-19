5e418890622fa89521b263c6f48dd5c94c37f075
Oleskii Verteletskyi <vertya@gmail.com>
2017-06-20 02:42:42 +0300

    init

---
+++ consul.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"encoding/base64"
+)
+
+func GetConfigFromConsul(url, key, token string) (configContent []byte, err error) {
+	req, err := http.NewRequest("GET", url+"/v1/kv/"+key, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("X-Consul-Token", token)
+	cl := http.DefaultClient
+	resp, err := cl.Do(req)
+	if err != nil {
+		return nil, err
+	} else {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		consulKey := make([]ConsulKey, 1)
+		err = json.Unmarshal(body, &consulKey)
+		if err != nil {
+			return nil, err
+		}
+		var keyValueContent []byte = make([]byte, len(consulKey[0].Value)*3/4)
+		_, err = base64.StdEncoding.Decode(keyValueContent, []byte(consulKey[0].Value))
+		if err != nil {
+			return nil, err
+		}
+		return keyValueContent, nil
+	}
+}
+
+type ConsulKey struct {
+	Key         string
+	CreateIndex uint64
+	ModifyIndex uint64
+	LockIndex   uint64
+	Flags       uint64
+	Value       string
+	Session     string
+}
---
+++ prop.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"unicode"
+	"io"
+	"log"
+)
+
+func ParseProperties(properties []byte) (map[string]string, error) {
+	scanner := bufio.NewScanner(bytes.NewBuffer(properties))
+	cfg := make(map[string]string)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		} else {
+			keyvalue := strings.Split(line, "=")
+			if len(keyvalue) >= 2 {
+				key := strings.TrimSpace(keyvalue[0])
+				value := strings.TrimSpace(line[len(key)+1:])
+				var tmp []rune
+				for _, r := range value {
+					if unicode.IsPrint(r) {
+						tmp = append(tmp, r)
+					}
+				}
+				cfg[key] = string(tmp)
+			}
+		}
+	}
+	err := scanner.Err()
+	if err != nil && err != io.EOF {
+		log.Println("save.preorder:error while read input data", err)
+		return nil, err
+	}
+	return cfg, nil
+}
